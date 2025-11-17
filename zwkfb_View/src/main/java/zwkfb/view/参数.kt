1c9015a0970cf7c8e688a0711b9fc1642c658cc7
得闲饮茶 <3293666408@qq.com>
2025-11-17 23:49:52 +0800

    Initial commit

--- zwkfb_View/src/main/java/zwkfb/view/参数.kt
+++ zwkfb_View/src/main/java/zwkfb/view/参数.kt
@@ -4,6 +4,9 @@ fun 测试(args: Array<String>) {
     println("参数个数：${args.size}")
 }
 
+fun 测试(args: List<String> = emptyList(), a: Int = 0) {
+    println("参数个数：${args.size}，a：$a")
+}
 
 fun main(args: Array<String>) {
     测试(args)
